111f9d7cc8698404608a961cac81ce600e41ecba
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-05-14 22:11:20 +0600

    complete basic stream setup

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,28 +1,55 @@
 "use client";
 
-import { StreamChat } from "stream-chat";
 import {
   Channel,
   ChannelHeader,
+  ChannelList,
   Chat,
+  LoadingIndicator,
   MessageInput,
   MessageList,
   Thread,
   Window,
 } from "stream-chat-react";
+import useInitializeChatClient from "./useInitializeChatClient";
+import { useUser } from "@clerk/nextjs";
 
 const ChatPage = () => {
+  const chatClient = useInitializeChatClient();
+  const { user } = useUser();
+
+  if (!chatClient || !user) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <LoadingIndicator size={40} />
+      </div>
+    );
+  }
   return (
-    <div>
+    <div className="h-screen">
       <Chat client={chatClient}>
-        <Channel channel={channel}>
-          <Window>
-            <ChannelHeader />
-            <MessageList />
-            <MessageInput />
-          </Window>
-          <Thread />
-        </Channel>
+        <div className="flex h-full flex-row">
+          <div className="w-full max-w-[360px]">
+            <ChannelList
+              filters={{
+                type: "messaging",
+                members: { $in: [user.id] },
+              }}
+              sort={{ last_message_at: -1 }}
+              options={{ state: true, presence: true, limit: 10 }}
+            />
+          </div>
+          <div className="size-full">
+            <Channel>
+              <Window>
+                <ChannelHeader />
+                <MessageList />
+                <MessageInput />
+              </Window>
+              <Thread />
+            </Channel>
+          </div>
+        </div>
       </Chat>
     </div>
   );
